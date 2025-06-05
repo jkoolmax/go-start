13ffe0aad3b413cbbd0042065e4fb0a2855ff1f8
nzembo <jkula.gov@gmail.com>
2025-06-04 23:57:45 -0500

    generics in go

--- Ducking.go
+++ Ducking.go
@@ -9,6 +9,10 @@ type Bird struct {
    age int
 }
 
+func (ss Bird) String() string {
+    return ss.name
+}
+
 func (b Bird) quack() string {
    if b.canFly == true && b.name =="duck" {
 	return "can fly and quack"
@@ -24,7 +28,9 @@ func main() {
   
    duck := Bird{name: "duck", canFly: true, age:2}
    turkey := Bird{name: "turkey", canFly: false, age: 1}
-
+   
+   //fmt.Println(duck) using Stringer e.g __str__
+   
    fmt.Printf("%v age: %d, %v\n", duck.name, duck.getAge(), duck.quack()) 
    fmt.Printf("%v age: %d, %v\n", turkey.name, turkey.getAge(), turkey.quack())
  
